74fc4c6f7ab4c63f6b2e225e618c9db195959483
Karen Agudelo Villa <agudelok838@gmail.com>
2026-02-04 17:20:45 -0500

    feat (config): add text ui

---
+++ src/constants/uiText.ts
@@ -0,0 +1,12 @@
+export const UI_TEXT = {
+  APP_TITLE: "Rick & Morty List",
+  SEARCH_PLACEHOLDER: "Search or filter results",
+  FILTERED_DATA: "CHARACTERS FOUND",
+  FAVORITES_TITLE: "STARRED CHARACTERS",
+  
+  STATES: {
+    LOADING: "Loading...",
+    ERROR: "Something went wrong",
+    NO_RESULTS: "No characters found",
+  },
+} as const;
