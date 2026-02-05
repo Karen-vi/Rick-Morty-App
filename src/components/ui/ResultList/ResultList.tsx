c626eb6b0776a140a9ddfdee9606a3c05f258324
Karen Agudelo Villa <agudelok838@gmail.com>
2026-02-05 08:03:18 -0500

    feat(ResultsList): character list component

---
+++ src/components/ui/ResultList/ResultList.tsx
@@ -0,0 +1,17 @@
+import type { Character } from "../../../types/character";
+import { CharacterCard } from "../CharacterCard/CharacterCard";
+
+type ResultsListProps = {
+  characters: Character[];
+  onSelectCharacter: (id: string) => void;
+};
+
+export const ResultsList = ({ characters, onSelectCharacter }: ResultsListProps) => {
+  return (
+    <div >
+      {characters.map((c) => (
+        <CharacterCard key={c.id} character={c} onSelect={onSelectCharacter} />
+      ))}
+    </div>
+  );
+};
