76948c3ae7da3425225006cd05d4105d0dd9b2e1
Karen Agudelo Villa <agudelok838@gmail.com>
2026-02-05 08:00:08 -0500

    feat(CharacterCard): character card component

---
+++ src/components/ui/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,27 @@
+import type { Character } from "../../../types/character";
+
+type CharacterCardProps = {
+  character: Character;
+  onSelect: (id: string) => void;
+};
+
+export const CharacterCard = ({ character, onSelect }: CharacterCardProps) => {
+  return (
+    <div className=" rounded-lg cursor-pointer hover:bg-secondary-40 w-full md:w-1/3 flex justify-start border-t border-b border-secondary-50  p-1"
+    onClick={() => onSelect(character.id)}>
+        <div className=" p-3">
+        <div className="flex items-center space-x-4">
+          <img
+            src={character.image}
+            alt={character.name}
+            className="w-10 h-10 rounded-full"/>
+            <div>
+                <h2 className="font-bold text-lg">{character.name}</h2>
+                <p className="text-sm text-gray-600">{character.species}</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+  );
+};
